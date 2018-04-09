0be956828b5b42af5fe2c40a7313d7698c8c1aee
Long Le <lelong37@gmail.com>
2018-04-09 14:49:09 -0500

    Adding ContextFactory for Design-time DbContext Creation https://docs.microsoft.com/en-us/ef/core/miscellaneous/cli/dbcontext-creation

---
+++ Northwind.Data/NorthwindContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Northwind.Data.Models;
+
+namespace Northwind.Data
+{
+    public class NorthwindContextFactory : IDesignTimeDbContextFactory<NorthwindContext>
+    {
+        public NorthwindContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<NorthwindContext>();
+            optionsBuilder.UseSqlServer("Server=.;Database=Northwind;Trusted_Connection=True;");
+            return new NorthwindContext(optionsBuilder.Options);
+        }
+    }
+}
