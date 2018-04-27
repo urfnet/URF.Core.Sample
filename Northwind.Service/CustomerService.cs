1c5ff38d5e613f7ad43daea2fe0b71c61aac870d
Long Le <lelong37@gmail.com>
2018-04-27 01:17:24 -0500

    Extend IService<TEntity> sample, scope: service layer.

---
+++ Northwind.Service/CustomerService.cs
@@ -0,0 +1,25 @@
+using System.Threading;
+using Northwind.Data.Models;
+using URF.Core.Abstractions.Trackable;
+using URF.Core.Services;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System;
+
+// Sample to extend ProductService, scoped to only ProductService vs. application wide
+namespace Northwind.Service
+{
+  public class CustomerService : Service<Customers>, ICustomerService
+  {
+    public CustomerService(ITrackableRepository<Customers> repository) : base(repository)
+    {
+    }
+
+    // Example, adding synchronous Single method
+    public Customers Single(Expression<Func<Customers, bool>> predicate)
+    {
+      return this.Repository.Queryable().Single(predicate);
+    }
+  }
+}
---
+++ Northwind.Service/ICustomerService.cs
@@ -0,0 +1,9 @@
+using Northwind.Data.Models;
+using URF.Core.Abstractions.Services;
+
+namespace Northwind.Service
+{
+  public interface ICustomerService : IService<Customers>
+  {
+  }
+}
