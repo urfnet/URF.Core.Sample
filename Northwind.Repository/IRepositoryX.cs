b8bef9d204d896888b7a7dcf0641707adbad37fe
Long Le <lelong37@gmail.com>
2018-04-27 16:17:44 -0500

    Adding extensbility comments.

--- Northwind.Repository/IRepositoryX.cs
+++ Northwind.Repository/IRepositoryX.cs
@@ -3,9 +3,9 @@ using System.Threading;
 using TrackableEntities.Common.Core;
 using URF.Core.Abstractions.Trackable;
 
-// Example: extending IRepository<TEntity> and/or ITrackableRepository<TEntity>, scope: application-wide
+// Example: extending IRepository<TEntity> and/or ITrackableRepository<TEntity>, scope: application-wide across all IRepositoryX<TEntity>
 public interface IRepositoryX<TEntity>: ITrackableRepository<TEntity> where TEntity : class, ITrackable
 {
-  // Example: adding synchronous Find, scope: application wide for all repositories
+  // Example: adding synchronous Find, scope: application-wide
   TEntity Find(object[] keyValues, CancellationToken cancellationToken = default);
 }
